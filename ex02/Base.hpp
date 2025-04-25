21805a842168955139984b7602c0e96ee19fca22
Jihyun Jeong <jihyjeon@cx2r1s1.42seoul.kr>
2025-04-25 22:52:02 +0900

    ex02 init

---
+++ ex02/Base.hpp
@@ -0,0 +1,33 @@
+#ifndef BASE_HPP
+#define BASE_HPP
+
+#include <iostream>
+
+/* Color Sets */
+#define RESET   "\033[0m"
+#define BLACK   "\033[30m"
+#define RED     "\033[31m"
+#define GREEN   "\033[32m"
+#define YELLOW  "\033[33m"
+#define BLUE    "\033[34m"
+#define MAGENTA "\033[35m"
+#define CYAN    "\033[36m"
+#define WHITE   "\033[37m"
+#define GREY    "\033[38;5;250m"
+
+class Base {
+private:
+    /* member attributes */
+public:
+    /* Orthodox Canonical Form (OCF) */
+    virtual    ~Base();
+
+    /* getter & setter */
+
+    /* additional methods */
+
+    /* exception classes */
+};
+
+/* operators */
+#endif
